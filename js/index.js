90a70aa79c92dbf1c7225179233e4f2493f16a09
NATALIE_PODSEDLY <natalie.podsedly@uvm.edu>
2024-10-28 11:39:44 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+document.addEventListener (); {
+    const themeForm = document.getElementById('themeform');
+    const contentDiv = document.getElementById('content'); 
+}
+
+const Theme = [ FirstDayOfFall, Halloween, Thanksgiving ];
+
+const grabChangingTheme = () => {
+    const chooseTheme = chooseTheme();
+} 
+
+
+// Apply the selected theme on change
+themeForm.addEventListener('change'), () => {
+    contentDiv.classList.remove('FirstDayofFall', 'Halloween', 'Thanksgiving');
+} 
